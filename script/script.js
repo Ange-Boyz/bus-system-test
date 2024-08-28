0088b7698da6f7f19a98e711b6091defd1999642
ange-boyz <angeboyz@icloud.com>
2024-08-28 06:51:02 +0100

    i add the location card

--- script/script.js
+++ script/script.js
@@ -35,14 +35,28 @@ const noAccount = document.querySelector('.login-text3');
  const body = document.querySelector('.mybody');
 const signIn = document.querySelector('.signin');
 const cross = document.querySelector('.log-cross');
+const LocationCross = document.querySelector('.l-cross')
+const buyBtn = document.querySelector('.section1-left-btn')
+const buyBtnTwo = document.querySelector('.buy-btn')
 
  signIn.addEventListener("click", ()=>{
     body.classList.add("active-signin")
- })
+ });
 
  
  cross.addEventListener("click", ()=>{
     body.classList.remove("active-signin")
- })
+ });
+
+buyBtn.addEventListener("click", ()=>{
+   body.classList.add("location-active")
+});
+
+LocationCross.addEventListener("click", ()=>{
+   body.classList.remove("location-active")
+});
+
+
+
 
 
